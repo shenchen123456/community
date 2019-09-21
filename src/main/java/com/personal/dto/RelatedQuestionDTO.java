6e5ffe64769f6bc881a409da7e07c3167486310d
shenchen123456 <1097011337@qq.com>
2019-09-21 17:15:07 +0800

    '相关问题和热门问题'

---
+++ src/main/java/com/personal/dto/RelatedQuestionDTO.java
@@ -0,0 +1,10 @@
+package com.personal.dto;
+
+/**
+ * @Auther: Chen
+ * @Data: 2019/9/20
+ * @Description: com.personal.dto
+ * @Version: 1.0.0
+ */
+public class RelatedQuestionDTO {
+}
---
+++ src/main/java/com/personal/provider/UCloudProvider.java
@@ -0,0 +1,10 @@
+package com.personal.provider;
+
+/**
+ * @Auther: Chen
+ * @Data: 2019/9/19
+ * @Description: com.personal.provider
+ * @Version: 1.0.0
+ */
+public class UCloudProvider {
+}
